1a3baa7d18d88b7a4a17c75540d4fe94aa96b30a
Levi-George <lmeg00001@gmail.com>
2022-09-18 15:11:23 -0400

    Selection statement structure
    Selection statement structure has been added, needs code and need to figure out when to get operands.

--- Calculator/calc.py
+++ Calculator/calc.py
@@ -17,8 +17,24 @@ def divide(val1, val2):
 
 #main
 
+while(isintance(input, int)):
+    input = input("Enter a number")
+
 while(input != 5):
     input = input("Select your operation: \n1. Addition\n2. Subtraction\n3. Multiplication \n4. Divsion\n5. Quit")
 
     while(input != 1 or input != 2 or input != 3 or input != 4 or input != 5):
         #Force user to input valid data
+        input = input("Select your operation: \n1. Addition\n2. Subtraction\n3. Multiplication \n4. Divsion\n5. Quit")
+
+    if(input == 1)
+
+    elif(input == 2)
+
+    elif(input == 3)
+
+    elif(input == 4)
+
+    elif(input == 5)
+        print("Understandable, have a nice day")
+        exit()
