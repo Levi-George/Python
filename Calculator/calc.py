50a2303c310cca574fc0f97b15ac11aa9d6b8009
Levi-George <lmeg00001@gmail.com>
2022-10-09 15:21:54 -0400

    Removed comments, neater code

--- Calculator/calc.py
+++ Calculator/calc.py
@@ -1,7 +1,5 @@
-#add, subtract, multiply, divide
-#how do we do this? check for proper inputs or just force user to input values for any 
-#   function
 
+#function definitions
 def add(val1, val2):
     return str(val1+val2)
 
@@ -17,14 +15,15 @@ def divide(val1, val2):
 
     return str(val1 / val2)
 
-def handleStrExcep(operStr):
+#Ensure input is not str or invalid, must be integer
+def getValidIntput(operStr):
     isNumber = False
 
     while(not(isNumber)):
         try:
             inputVal = int(input("Enter your " + operStr + " operand: "))
             isNumber = True
-        except ValueError:
+        except ValueError: 
             print("ERROR -> Enter an appropriate value please (no words, letters, or special characters)\n", end = "")
             isNumber = False
 
@@ -33,11 +32,6 @@ def handleStrExcep(operStr):
 
 #main
 
-#Get inputs from user
-#Check to make sure inputs are valid
-
-#Learned: input function gets a string, we need to convert it
-
 #our input value
 inputVal = -1
 
@@ -45,6 +39,7 @@ while(inputVal != 5 and inputVal != "n" and inputVal != "N"):
 
     while(inputVal != 1 and inputVal != 2 and inputVal != 3 and inputVal != 4 and inputVal != 5):
         try:
+            #Learned: input function gets a string, we need to convert it
             inputVal = int(input("\nSelect your operation: \n1. Addition\n2. Subtraction\n3. Multiplication \n4. Divsion\n5. Quit\nResponse: "))
         except ValueError:
             print("Enter an appropriate value please (no words, letters, or special characters)\n")
@@ -53,12 +48,11 @@ while(inputVal != 5 and inputVal != "n" and inputVal != "N"):
             print("Understandable, have a nice day")
             exit()
 
-    operand1 = handleStrExcep("first")
-    operand2 = handleStrExcep("second")
 
-#Learned, you can't concat integers without modifying
+    operand1 = getValidIntput("first")
+    operand2 = getValidIntput("second")
 
-    if(inputVal == 1):
+    if(inputVal == 1): #Learned, you can't concat integers without modifying
         print("Your result of adding " + str(operand1) + " to " + str(operand2) + " is " + add(operand1, operand2))
     elif(inputVal == 2):
         print("Your result of subtracting " + str(operand1) + " from " + str(operand2) + " is " + subtract(operand1, operand2))
