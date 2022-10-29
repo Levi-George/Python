d092542b3ec4d306c8bc766b448ba971377fd2aa
Levi-George <lmeg00001@gmail.com>
2022-10-28 23:32:55 -0400

    Basic function finished
    Resolved, found several bugs and issues.
    -Formatting of HTTP response messages
    -Use of server socket instead of connection socket to recv()
    -read of file, file needs to read from, file iterator does not do this automatically?

--- Web Server Lab/WebServ.py
+++ Web Server Lab/WebServ.py
@@ -21,32 +21,48 @@ while True:
     connectionSocket, addr = serverSocket.accept()
 
     try:
-        message = serverSocket.recv(2048).decode()
+        message = connectionSocket.recv(1024).decode() #you had the server socket set to receive
         filename = message.split()[1]
-        f = open(filename[1:])
+        f = open(filename[1:], 'r')
 
-        outputdata = f
+        outputdata = f.read() #you need to set the file to read
 
         #Send one HTTP header line to socket
 
         httpHeader = 'HTTP/1.0 200 OK\r\n'
 
-        connectionSocket.send(httpHeader.encode(), addr)
+        connectionSocket.send(httpHeader.encode('iso-8859-1'))
+
+        httpHeader = 'Content-Type: text/html\n\r\n'
+
+        connectionSocket.send(httpHeader.encode('iso-8859-1'))
 
 
         #send content of the requested file to the client
         for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata[i].encode())
+            print(outputdata[i])
         connectionSocket.send("\r\n".encode())
 
         connectionSocket.close()
     except IOError:
-        #Send 404 response
-        #Fill In
-        httpHeader = 'HTTP/1.0 404 Page Not Found\r\n'
-
-        connectionSocket.send(httpHeader.encode(), addr)
+    #   Send 404 response
+    #   Fill In
         
+        outputdata = open("Hello404.html", 'r').read()
+
+        httpHeader = "HTTP/1.1 404 Page Not Found\r\n"
+        connectionSocket.send(httpHeader.encode('iso-8859-1'))
+
+        httpHeader = 'Content-Type: text/html\n\r\n'
+        connectionSocket.send(httpHeader.encode('iso-8859-1'))
+
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i].encode())
+        connectionSocket.send("\r\n".encode())
+
+        print(httpHeader)
+
         #Close Socket
         serverSocket.close()
 
