41effa46edae7731fccc0e0220fd20e9a3c64344
Sazen <georlm01@pfw.edu>
2022-11-01 14:44:52 -0400

    Basic Message Sending
    Send and then sleep to make output readable
    
    added output of message from client to server code

--- PingServer-Client/PingClient.py
+++ PingServer-Client/PingClient.py
@@ -1,1 +1,14 @@
 from socket import *
+import time
+# Create a UDP socket
+# Notice the use of SOCK_DGRAM for UDP packets
+sendSocket = socket(AF_INET, SOCK_DGRAM)
+# Assign IP address and port number to socket
+sendSocket.connect(('127.0.0.1', 12000))
+
+while(True):
+
+    sendSocket.send("Hello".encode('utf-8'))
+
+    time.sleep(2)
+
